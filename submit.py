8fa17d0fe48051247fdb2822dc15ef7afab651f4
egg988 <451681355@qq.com>
2024-09-21 16:07:57 +0800

    Update submit.py

--- submit.py
+++ submit.py
@@ -1,4 +1,9 @@
 import os
 
 os.system('git log -1 --pretty=format:"%h" > .git/CUR_COMMIT')
-os.system('zip -j submit.zip .git/CUR_COMMIT foggytcp/src/foggy_function.cc foggytcp/inc/foggy_function.h')
+os.system('zip -j submit.zip \
+          .git/CUR_COMMIT \
+          foggytcp/src/foggy_function.cc \
+          foggytcp/src/foggy_tcp.cc \
+          foggytcp/inc/foggy_function.h \
+          foggytcp/inc/foggy_tcp.h')
